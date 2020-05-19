7118cf2d00f488d3da9e079eea0778d60d233b06
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-19 16:25:04 -0600

    Rutas de login/registrado y login/admin creadas

--- routes/login-router.js
+++ routes/login-router.js
@@ -5,17 +5,18 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var SECRET_KEY ='secretkey123456';
 
-router.post('/',function(req,res){
+router.post('/admin/:idRol',function(req,res){
     userData ={
         correo: req.body.correo,
         contra:req.body.contra
     }
     usuario.findOne(
-        {correo:userData.correo}
+        {correo:userData.correo,rol:req.params.idRol}
     ).then(result=>{
+        console.log(result)
         if(!result){
             //usuario no existe
-            res.send({mensaje:'-1',resultado:result});
+            res.send({mensaje:'Usuario Invalido',resultado:result});
         }else{
             const resultContrasena = bcrypt.compareSync(userData.contra, result.contrasena)
             if(resultContrasena){
@@ -40,4 +41,39 @@ router.post('/',function(req,res){
     });
 });
 
+router.post('/registrado/:idRol',function(req,res){
+    userData ={
+        correo: req.body.correo,
+        contra:req.body.contra
+    }
+    usuario.findOne(
+        {correo:userData.correo,rol:req.params.idRol}
+    ).then(result=>{
+        
+        if(!result){
+            //usuario no existe
+            res.send({mensaje:'Usuario Invalido',resultado:result});
+        }else{
+            const resultContrasena = bcrypt.compareSync(userData.contra, result.contrasena)
+            if(resultContrasena){
+                const expiresIn = 24*60*60;
+                const accessToken = jwt.sign({id:result._id},SECRET_KEY,{expiresIn:expiresIn}); 
+                const dataUser ={
+                    id:result._id,
+                    nombres:result.nombres,
+                    accessToken:accessToken,
+                    expiresIn:expiresIn
+                }
+                res.send(dataUser);
+                
+            }else{
+                //contraseña  incorrecta
+                res.send({mensaje:'incorrecta'});
+            }    
+        }
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    });
+});
 module.exports = router;
