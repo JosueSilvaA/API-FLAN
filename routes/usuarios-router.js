1c028c1003cc00b4aee8a5deaf161e10fd42334f
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-19 01:04:39 -0600

    ya

--- routes/usuarios-router.js
+++ routes/usuarios-router.js
@@ -104,7 +104,7 @@ router.post('/login',function(req,res){
                 res.send({mensaje:'funciona'})
             }else{
 
-                res.send({mensaje:'no es la ',info:userData.contrasena})
+                res.send({mensaje:'no es la ',info:result.contrasena})
             }
             
         }
