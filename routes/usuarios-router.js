5d7329b806148f56037df62fb80457000e3fe6d9
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-18 23:54:27 -0600

    Arreglo servicio login

--- routes/usuarios-router.js
+++ routes/usuarios-router.js
@@ -85,43 +85,18 @@ router.delete('/:id',function(req,res){
 });
 
 router.post('/login',function(req,res){
-    userData ={
-        correo: req.body.correo,
-        contrasena:req.body.contrasena
-    }
     usuario.findOne(
         {
-            correo: userData.correo
+            correo:req.body.correo
         },
         {_id:true,nombres:true,contrasena:true}
     ).then(result=>{
-        if(!result){
-            //usuario no existe
-            res.send({mensaje:'-1',resultado:result});
-            res.end();
-        }else{
-            const resultContrasena = bcrypt.compareSync(userData.contrasena, result.contrasena)
-            if(resultContrasena){
-                const expiresIn = 24*60*60;
-                const accessToken = jwt.sign({id:result._id},SECRET_KEY,{expiresIn:expiresIn}); 
-                const dataUser ={
-                    id:result._id,
-                    nombres:result.nombres,
-                    accessToken:accessToken,
-                    expiresIn:expiresIn
-                }
-                res.send(result);
-                res.end();
-            }else{
-                //contraseña  incorrecta
-                res.send({mensaje:'incorrecta',resultado:result});
-                res.end();
-            }    
-        }
+        res.send(result);
+        res.end();
     }).catch(error=>{
         res.send(error);
         res.end();
     });
-});
+})
 
 module.exports = router;
