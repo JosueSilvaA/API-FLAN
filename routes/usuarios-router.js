3ffae020dfdc1adf065d650acdc209be1632baa0
JosueSilvaA <josuemarkcs@gmail.com>
2020-05-19 00:11:53 -0600

    Arreglo servicio login

--- routes/usuarios-router.js
+++ routes/usuarios-router.js
@@ -106,7 +106,7 @@ router.post('/login',function(req,res){
                     accessToken:accessToken,
                     expiresIn:expiresIn
                 }
-                res.send({mensaje:'existe',resultado:result});
+                res.send(result);
 
             }else{
                 //contraseña  incorrecta
